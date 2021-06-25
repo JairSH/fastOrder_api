fbe16cb60e557e3e9337520a5f1aa2c353a2f6a9
jair <jair@hotmail.com>
2021-06-24 20:37:13 -0500

    modification in url file

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,7 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('fastOrder.users.urls', 'users'), namespace='users')),
+    path('', include(('fastorder.menus.urls', 'menus'), namespace='menus')),
+    path('', include(('fastorder.ordenes.urls', 'ordenes'), namespace='ordenes')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
